9792820a9a28fec754a7e8030a40ad96aa646e96
cyrilvernier <cyril@nanostud.io>
2024-03-13 15:55:56 +0100

    Update settings.js to add font partial

--- settings.js
+++ settings.js
@@ -9,6 +9,7 @@ module.exports = {
         { path: "/src/css", type: "directory" },
         { path: "/src/css/modules", type: "directory" },
         { path: "/src/css/main.css", type: "file" },
+        { path: "/src/css/_font.css", type: "file" },
         { path: "/src/css/_base.css", type: "file" },
         { path: "/src/css/_layout.css", type: "file" },
         { path: "/src/css/modules/_example.css", type: "file" },
@@ -23,6 +24,7 @@ module.exports = {
         { path: "/src/scss", type: "directory" },
         { path: "/src/scss/modules", type: "directory" },
         { path: "/src/scss/main.scss", type: "file" },
+        { path: "/src/scss/_font.scss", type: "file" },
         { path: "/src/scss/_base.scss", type: "file" },
         { path: "/src/scss/_layout.scss", type: "file" },
         { path: "/src/scss/modules/_example.scss", type: "file" },
@@ -32,11 +34,11 @@ module.exports = {
   import_rules: {
     css: {
       path: "/src/css/main.css",
-      data: '@import "./_base.css";\n@import "./_layout.css";\n@import "./modules/_example.css";',
+      data: '@import "./_font.css";\n@import "./_base.css";\n@import "./_layout.css";\n@import "./modules/_example.css";',
     },
     scss: {
       path: "/src/scss/main.scss",
-      data: '@import "base";\n@import "layout";\n@import "modules/example";',
+      data: '@import "font";\n@import "base";\n@import "layout";\n@import "modules/example";',
     },
   },
   reset_rules: {
